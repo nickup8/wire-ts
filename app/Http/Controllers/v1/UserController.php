b6552d4d41b590836e5b09bf76822d0bad7fe969
nickup8 <nickup8@yandex.ru>
2023-08-29 05:39:14 +0300

    Реализован вывод пользователей из БД

---
+++ app/Http/Controllers/v1/UserController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\v1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use App\Http\Resources\UserResource;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        return (UserResource::collection(User::all()));
+    }
+}
